fde794e0ebce5159eb8f0ae419bc43cc5b1676b5
monkeontheroof <khangkhoailang20@gmail.com>
2023-11-15 00:17:23 +0700

    14/11/2023 - 11:34pm

--- src/main/java/com/group5/ecommerce/controller/LoginController.java
+++ src/main/java/com/group5/ecommerce/controller/LoginController.java
@@ -8,6 +8,7 @@ import com.group5.ecommerce.utils.CartUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,7 +30,8 @@ public class LoginController {
     private RoleRepository roleRepository;
 
     @GetMapping("/login")
-    public String login(){
+    public String login(Model model){
+        model.addAttribute("user", new User());
         return "clients/sign-in";
     }
 
