98b7d51c2195c304d210c8b8d1cb16a8f77f10eb
monkeontheroof <khangkhoailang20@gmail.com>
2023-11-15 00:24:16 +0700

    14/11/2023 - 11:34pm

---
+++ src/main/java/com/group5/ecommerce/model/RegistrationForm.java
@@ -0,0 +1,10 @@
+package com.group5.ecommerce.model;
+
+public class RegistrationForm {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String confirmPassword;
+}
